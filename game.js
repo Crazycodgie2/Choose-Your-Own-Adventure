b5c5da93ea549a2d8e536319c8ac40507e2809e8
Colin Littlejohn <crazycodgie13@gmail.com>
2020-01-27 13:32:39 -0800

    initial commit

---
+++ game.js
@@ -0,0 +1,12 @@
+prompt("Hello. What is your name?")
+
+var name = prompt("Hello. What is your name?")
+if (name) {
+  prompt(
+    `Nice to meet you ${name}. Your adventure starts at a crossroads. There is a path to the left, right, and backwards. Which way do you go?`
+  )
+} else {
+  alert("Very well, goodbye.")
+}
+
+var left = alert("You go down the left path")
