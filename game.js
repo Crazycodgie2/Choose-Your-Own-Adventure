3d6661b1028a41e289715d28d221e3e6495eedfe
Colin Littlejohn <crazycodgie13@gmail.com>
2020-01-27 14:27:54 -0800

    Initial start

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
-prompt("Hello. What is your name?")
-
 var name = prompt("Hello. What is your name?")
+
 if (name) {
   prompt(
     `Nice to meet you ${name}. Your adventure starts at a crossroads. There is a path to the left, right, and backwards. Which way do you go?`
@@ -8,5 +7,3 @@ if (name) {
 } else {
   alert("Very well, goodbye.")
 }
-
-var left = alert("You go down the left path")
